17c201c33eb8e9d5799c23a72e9a9581b02ce36c
namke-dev <namke.dev@gmail.com>
2024-01-06 04:51:00 +0700

    Add stock API

---
+++ src/api/stock-api.js
@@ -0,0 +1,67 @@
+const basePath = "https://finnhub.io/api/v1";
+
+export const searchSymbols = async (query) => {
+  const url = `${basePath}/search?q=${query}&token=${process.env.REACT_APP_API_KEY}`;
+
+  console.log("searchSymbols:" + url);
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+  }
+
+  return await response.json();
+};
+
+export const fetchStockDetails = async (stockSymbol) => {
+  const url = `${basePath}/stock/profile2?symbol=${stockSymbol}&token=${process.env.REACT_APP_API_KEY}`;
+
+  console.log("fetchStockDetails:" + url);
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+  }
+
+  return await response.json();
+};
+
+/**
+ * Fetches the latest quote of a given stock
+ * @param {string} stockSymbol - Symbol of the company, e.g. 'FB'
+ * @returns {Promise<Object>} Response object
+ */
+export const fetchQuote = async (stockSymbol) => {
+  const url = `${basePath}/quote?symbol=${stockSymbol}&token=${process.env.REACT_APP_API_KEY}`;
+
+  console.log("fetchQuote:" + url);
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+  }
+
+  return await response.json();
+};
+
+export const fetchHistoricalData = async (
+  stockSymbol,
+  resolution,
+  from,
+  to
+) => {
+  const url = `${basePath}/stock/candle?symbol=${stockSymbol}&resolution=${resolution}&from=${from}&to=${to}&token=${process.env.REACT_APP_API_KEY}`;
+
+  console.log("fetchHistoricalData:" + url);
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+  }
+
+  return await response.json();
+};
