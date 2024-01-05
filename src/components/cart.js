031ea6b1433eb5a6f0cafea5317545d721c1565d
namke-dev <namke.dev@gmail.com>
2024-01-06 00:47:43 +0700

    Change card css

--- src/components/cart.js
+++ src/components/cart.js
@@ -3,8 +3,10 @@ import React from "react";
 export default function Cart({ children }) {
   return (
     <div
-      className="h-full w-full rounded-md relative p8
-        border-2 bg-gray-300"
+      className="h-full w-full rounded-md relative p-8
+        border-2 
+        border-neutral-200
+        bg-white "
     >
       {children}
     </div>
