938b74250f65407ef5b2eff0ffbe357d361f47db
namke-dev <namke.dev@gmail.com>
2024-01-06 02:26:46 +0700

    add chart color

--- src/components/dashboard-chart.js
+++ src/components/dashboard-chart.js
@@ -27,12 +27,23 @@ export default function DashboardChart() {
     <Cart>
       <ResponsiveContainer>
         <AreaChart data={formData(data)}>
+          <defs>
+            <linearGradient id="chartColor" x1="0" y1="0" x2="0" y2="1">
+              <stop
+                offset="5%"
+                stopColor="rgb(199 210 254)"
+                stopOpacity={0.8}
+              />
+              <stop offset="95%" stopColor="rgb(199 210 254)" stopOpacity={0} />
+            </linearGradient>
+          </defs>
           <Area
             type="monotone"
             dataKey="value"
             stroke="#312e81"
             fillOpacity={1}
             strokeWidth={0.5}
+            fill="url(#chartColor)"
           />
           <Tooltip />
           <XAxis dataKey={"date"} />
