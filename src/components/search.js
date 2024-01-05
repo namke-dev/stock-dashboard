001d23ac057140a30b1b7746fb4f27db3f00030f
namke-dev <namke.dev@gmail.com>
2024-01-05 23:45:58 +0700

    add input box

--- src/components/search.js
+++ src/components/search.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { mockSearchResult } from "../constants/mock";
 import { XIcon, SearchIcon } from "@heroicons/react/solid";
 export default function Search() {
-  const [input, setInput] = useState();
+  const [input, setInput] = useState("");
   const [bestMatches, setBestMatches] = useState(mockSearchResult.result);
 
   const clear = () => {
@@ -25,20 +25,34 @@ export default function Search() {
         bg-white
         border-neutral-200"
     >
+      {/* input search term */}
       <input
         type="text"
         value={input}
         className=" w-full px-4 py-2 focus:outline-none rounded-md"
         placeholder="Search stock..."
         onChange={(event) => {
+          setInput(event.target.value);
+        }}
+        onKeyDown={(event) => {
           if (event.key === "Enter") {
             updateBestMatches();
           }
         }}
       />
 
-      <button>
-        <XIcon className="h-4 w-4 fill-gray-500" />
+      {/* Clear button */}
+      {input && (
+        <button onClick={clear}>
+          <XIcon className="h-4 w-4 fill-gray-500" />
+        </button>
+      )}
+
+      <button
+        onClick={updateBestMatches}
+        className="h-8 w-8 bg-indigo-600 rounded-md flex justify-center items-center m-1 p-2"
+      >
+        <SearchIcon className="h-4 w-4 fill-gray-200" />
       </button>
     </div>
   );
