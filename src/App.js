d711177094def68116e962aceab3bf0cc16c9768
namke-dev <namke.dev@gmail.com>
2024-01-06 03:48:30 +0700

    Create Theme context

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { useState } from "react";
 import "./App.css";
 import Dashboard from "./components/dashboard";
+import ThemeContext from "./context/theme-context";
 
 function App() {
-  return <Dashboard></Dashboard>;
+  const [darkMode, setDarkMode] = useState(false);
+  return (
+    <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
+      <Dashboard />
+    </ThemeContext.Provider>
+  );
 }
 
 export default App;
--- src/components/theme-icon.js
+++ src/components/theme-icon.js
@@ -1,13 +1,28 @@
 import { MoonIcon } from "@heroicons/react/solid";
-import React from "react";
+import React, { useContext } from "react";
+import ThemeContext from "../context/theme-context";
 
 export default function ThemeIcon() {
+  const { darkMode, setDarkMode } = useContext(ThemeContext);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
   return (
     <button
-      className="rounded-lg border-1 border-neutral-200 p-0
-        absolute right-8 xl:right-32 shadow-lg"
+      className={`rounded-lg border-1 border-neutral-200 p-1.5
+      absolute right-8 xl:right-32 shadow-lg
+      ${darkMode ? "shadow-gray-800" : null}`}
+      onClick={toggleDarkMode}
     >
-      <MoonIcon className="h-8 w-8 cursor-pointer stroke-1 stroke-neutral-400 fill-none"></MoonIcon>
+      <MoonIcon
+        className={`h-7 w-8 cursor-pointer stroke-1
+        ${
+          darkMode
+            ? "fill-yellow-400 stroke-yellow-400"
+            : " stroke-neutral-400 fill-none"
+        }`}
+      />
     </button>
   );
 }
