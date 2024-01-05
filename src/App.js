1425f9a75d3d3ab366340959d4e5015fad13c1b8
namke-dev <namke.dev@gmail.com>
2024-01-06 04:50:47 +0700

    Create StockContext

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import { useState } from "react";
 import "./App.css";
 import Dashboard from "./components/dashboard";
 import ThemeContext from "./context/theme-context";
+import StockContext from "./context/stock-context";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
+  const [stockSymbol, setStockSymbol] = useState("FB");
+
   return (
     <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
-      <Dashboard />
+      <StockContext.Provider value={{ stockSymbol, setStockSymbol }}>
+        <Dashboard />
+      </StockContext.Provider>
     </ThemeContext.Provider>
   );
 }
